a911beac41ae0906a1993d635b2b7057c4588d6c
SetiyoPramono <setiyopramono04@gmail.com>
2023-02-10 11:37:08 +0700

    add bootsttrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import '../public/dist/css/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
