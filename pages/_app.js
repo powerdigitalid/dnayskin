bdd4bc7714ab526e2b91f9f4a95b941adfbf9f71
veralindha <fangkyistiawan@gmail.com>
2023-02-16 23:05:15 +0700

    'fixed-font'

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../public/dist/css/main.css'
 import '../public/dist/css/bootstrap.css'
 import '../public/dist/css/tiny-slider.css'
 import '../public/dist/css/glightbox.css'
+import '../public/dist/css/lineicons.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
