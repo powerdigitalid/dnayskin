1dbf07e6f01797e0ef9f1788e32a3544900ab41e
veralindha <fangkyistiawan@gmail.com>
2023-03-02 15:29:34 +0700

    add folder admin menu

--- components/admin-pages/utils/navbar.js
+++ components/admin-pages/utils/navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <div>
       <div className="navbar-bg"></div>
-      <nav className="navbar navbar-expand-lg main-navbar">
+      <nav className="navbar main-navbar">
         <form className="form-inline mr-auto">
           <ul className="navbar-nav mr-3">
             <li><a href="#" data-toggle="sidebar" className="nav-link nav-link-lg"><i className="fas fa-bars" /></a></li>
--- components/home-pages/utils/layout.js
+++ components/home-pages/utils/layout.js
@@ -1,25 +1,23 @@
-import Head from 'next/head'
-import React from 'react'
-import Favicon from '../../../public/dist/img/logo/logo.svg'
-import Footer from './footer'
-import Navbar from './navbar'
+import Head from "next/head";
+import React from "react";
+import Favicon from "../../../public/dist/img/logo/logo.svg";
+import Footer from "./footer";
+import Navbar from "./navbar";
 
-
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div>
-        <Head>
+      <Head>
         <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Rumah Athan | Beauty Center</title>
-    <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" type="image/x-icon" href={Favicon} />
-        </Head>
-        <Navbar />
-        {children}
-        <Footer />
-        
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <title>Rumah Athan | Beauty Center</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" type="image/x-icon" href={Favicon} />
+      </Head>
+      <Navbar />
+      {children}
+      <Footer />
     </div>
-  )
+  );
 }
