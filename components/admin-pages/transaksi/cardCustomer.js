6e2e7d0cca2c148de2e8c37be9ec4a17e1c88c5d
SetiyoPramono <setiyopramono04@gmail.com>
2023-03-04 11:26:54 +0700

    add cardCustomer

---
+++ components/admin-pages/transaksi/cardCustomer.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export default function CardCustomer() {
+  return (
+    <>
+      <div className="card author-box card-primary">
+        <div className="card-body">
+          <div className="col-12">
+            <div className="text-center text-dark ">
+              <h2>Order</h2><hr className='mx-auto rounded custom-hr'></hr>
+            </div>
+          </div>
+          <div className="author-box-left">
+            <img
+              alt="image"
+              src="/dist/img/products/product-1.jpg"
+              className="rounded author-box-picture"
+            />
+            <div className="clearfix" />
+          </div>
+          <div className="author-box-details">
+            <div className="author-box-name">
+              <div className="form-group">
+                <div className="form-row">
+                  <div className="form-group col-sm-6">
+                    <label>Nama Lengkap</label>
+                    <input
+                      type="text"
+                      className="form-control form-control-sm"
+                      disabled
+                    />
+                  </div>
+                  <div className="form-group col-sm-6">
+                    <label>Nomer Handphone</label>
+                    <div className="input-group input-group-sm">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">+62</span>
+                      </div>
+                      <input
+                        type="text"
+                        className="form-control form-control-sm"
+                        aria-label="Nomer HP"
+                        disabled
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="form-group col-sm-12">
+                    <label>Alamat</label>
+                    <textarea class="form-control" disabled></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ pages/transaksipages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CardCustomer from '../../components/admin-pages/transaksi/cardCustomer'
+import Layout from '../../components/admin-pages/utils/layout'
+
+export default function index() {
+  return (
+    <>
+    <Layout>
+      <CardCustomer/>
+    </Layout>
+    </>
+  )
+}
