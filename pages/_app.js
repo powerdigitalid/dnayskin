d4bc32c5db2dd847d51e119d292eada0260bcc28
SetiyoPramono <setiyopramono04@gmail.com>
2023-03-04 11:36:52 +0700

    repairing _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import '../public/dist/css/main.css'
-import '../public/dist/css/bootstrap.css'
 import '../public/dist/css/tiny-slider.css'
 import '../public/dist/css/glightbox.css'
 import '../public/dist/css/lineicons.css'
