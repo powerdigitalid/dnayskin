242b0992f5ce942bfb58d55e2c4ee9ee3ce1fabc
AlvifSandana <asm240800@gmail.com>
2023-03-23 13:22:05 +0700

    [20230323]: detail transaksi (NEED REVIEW)

--- components/admin-pages/transaksi/detailTransaksi.js
+++ components/admin-pages/transaksi/detailTransaksi.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { getCookie } from "../../../utils/cookie.util";
+import { useRouter } from "next/router";
 export default function DetailTransaksi() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [order, setOrder] = useState({});
+  const handleFetchOrder = () => {
+    fetch(`${process.env.NEXT_PUBLIC_API_DEV}order/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Access-Token": getCookie("token")
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if(res.data){
+          setOrder(res.data);
+          console.log(res.data)
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    handleFetchOrder();
+  }, []);
   return (
     <>
       <div className="card author-box card-primary">
@@ -19,33 +46,37 @@ export default function DetailTransaksi() {
                   type="text"
                   className="form-control"
                   disabled
-                  value={"Nama Aku"}
+                  value={order.customerName ? order.customerName : ""}
                 />
               </div>
             </div>
           </div>
           <div className="row">
             <label className="form-group col-12 text-primary m-0">
-              Product & Treatment
+              Products & Treatments
             </label>
             <div
               className="form-group col-12 overflow-auto w-auto"
               style={{ height: "100px" }}
             >
-              <label>1. Produk Nomor 1</label>
-              <br />
-              <label>1. Produk Nomor 1</label>
-              <br />
-              <label>1. Produk Nomor 1</label>
-              <br />
-              <label>1. Produk Nomor 1</label>
-              <br />
-              <label>1. Produk Nomor 1</label>
-              <br />
-              <label>1. Produk Nomor 1</label>
-              <br />
-              <label>1. Produk Nomor 1</label>
-              <br />
+              <table className="table table-sm table-hover" >
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Products & Treatments</th>
+                    <th className="text-center">Harga (Rp)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {order.order_detail ? (order.order_detail.map((product, i) => (
+                    <tr key={i}>
+                      <td>{i+1}</td>
+                      <td>{product.product_name}</td>
+                      <td className="text-right">{product.price}</td>
+                    </tr>
+                  ))) : (<tr><td className="text-center text-warning" colSpan={3}>Belum ada produk yang ditambahkan</td></tr>)}
+                </tbody>
+              </table>
             </div>
           </div>
           <div className="row">
@@ -56,13 +87,13 @@ export default function DetailTransaksi() {
                   type="text"
                   className="form-control"
                   disabled
-                  value={"Kantor Songgon"}
+                  value={order.officeId ? order.officeId : ""}
                 />
               </div>
             </div>
             <div className="form-group col-6">
               <label>Tanggal</label>
-              <input type="date" className="form-control" disabled />
+              <input type="text" className="form-control" value={order.order_date ? new Date(order.order_date).toLocaleString() : ""} disabled />
             </div>
           </div>
           <div className="row">
@@ -72,7 +103,7 @@ export default function DetailTransaksi() {
                 <textarea
                   className="form-control"
                   disabled
-                  value={"mengalami kerutan wajah"}
+                  value={order.anamnesa ? order.anamnesa : ""}
                 />
               </div>
               <div className="form-group">
@@ -80,11 +111,11 @@ export default function DetailTransaksi() {
                 <textarea
                   className="form-control"
                   disabled
-                  value={"ada tuma"}
+                  value={order.diagnosa ? order.diagnosa : ""}
                 />
               </div>
               <div className="form-group">
-                <label>Total Pesanan : Rp. 500.000</label>
+                <label>Total Pesanan : Rp. {order.order_total ? order.order_total : "0"}</label>
               </div>
               <div className="form-group">
                 <Link href="/admin/transaksipages" className="btn btn-danger">
