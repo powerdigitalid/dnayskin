fc7e4fbe1410822693996771de751711e60a58f9
veralindha <fangkyistiawan@gmail.com>
2023-04-06 21:35:52 +0700

    add pavicon

--- components/admin-pages/utils/layout.js
+++ components/admin-pages/utils/layout.js
@@ -1,20 +1,29 @@
 import Footer from "./footer";
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
-    <div id="app">
-      <div className="main-wrapper main-wrapper-1">
-        <Navbar />
-        <Sidebar />
-        <div className="main-content" style={{ minHeight: '530' }}>
-          <section className="section">
-            {children}
-          </section>
+    <div>
+      <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <title>Admin | Rumah Athan</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="/dist/img/logo/logos.png" />
+      </Head>
+      <div id="app">
+        <div className="main-wrapper main-wrapper-1">
+          <Navbar />
+          <Sidebar />
+          <div className="main-content" style={{ minHeight: "530" }}>
+            <section className="section">{children}</section>
+          </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
     </div>
-  )
+  );
 }
